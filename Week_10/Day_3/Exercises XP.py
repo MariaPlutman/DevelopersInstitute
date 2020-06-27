ab145a3c4bfb0c7998044e0c1a43fe6eb13ce090
MariaPlutman <mariaplutman@gamil.com>
2020-06-27 16:38:31 +0300

    Exercises XP

--- Week_10/Day_3/Exercises XP.py
+++ Week_10/Day_3/Exercises XP.py
@@ -1,10 +1,20 @@
 import json
-from collections import OrderedDict
 
 # Exercise 1
-with open("/Users/user/Desktop/DevelopersInstitute/Week_10/Day_3/ex1.json", "r") as file:
-    data = json.load(file)
-    print(data["company"]["employee"]["payble"]["salary"])
+sampleJson = """{ 
+   "company":{ 
+      "employee":{ 
+         "name":"emma",
+         "payble":{ 
+            "salary":7000,
+            "bonus":800
+         }
+      }
+   }
+} """
+
+data = json.loads(sampleJson)
+print(data["company"]["employee"]["payble"]["salary"])
 
 # Exercise 2
 with open("/Users/user/Desktop/DevelopersInstitute/Week_10/Day_3/ex2.json", "w") as file:
