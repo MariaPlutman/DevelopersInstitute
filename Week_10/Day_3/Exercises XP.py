4088982a6181da5d635791559c4ec63a856d1917
MariaPlutman <mariaplutman@gamil.com>
2020-06-27 16:51:44 +0300

    Exercises XP

--- Week_10/Day_3/Exercises XP.py
+++ Week_10/Day_3/Exercises XP.py
@@ -18,10 +18,13 @@ print(data["company"]["employee"]["payble"]["salary"])
 
 # Exercise 2
 with open("/Users/user/Desktop/DevelopersInstitute/Week_10/Day_3/ex2.json", "w") as file:
-    sampleJson = {"id": 1, "name": "value2", "age": 29}
-    dict_keys = sampleJson.keys()
-    dict_keys = sorted(dict_keys)
-    new_dict = {}
-    for key in dict_keys:
-        new_dict[key] = sampleJson[key]
-    json.dump(new_dict, file)
+    sampleJson2 = """{"id": 1, "name": "value2", "age": 29}"""
+    data = json.loads(sampleJson2)
+    sorted_json = json.dumps(data, indent=2, sort_keys=True)
+    file.write(sorted_json)
+    # dict_keys = sampleJson.keys()
+    # dict_keys = sorted(dict_keys)
+    # new_dict = {}
+    # for key in dict_keys:
+    #     new_dict[key] = sampleJson[key]
+    # json.dump(new_dict, file)
